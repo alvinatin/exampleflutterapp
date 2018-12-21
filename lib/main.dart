2a608f63483d7e33ec0e1610accc39318d9166f6
alvinatin <alvin2304@gmail.com>
2018-12-21 14:31:56 +0700

    aaaa

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -38,11 +39,12 @@ class MyHomeState extends State<HomePageWidget> {
       appBar: AppBar(
         title: Text('Platform Channel'),
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: _showNativeView,
-          child: Text("Show Native View"),),
-      ),
+//      body: Center(
+//        child: RaisedButton(
+//          onPressed: _showNativeView,
+//          child: Text("Show Native View"),),
+//      ),
+    body: RandomWords(),
     );
   }
 
@@ -58,3 +60,64 @@ class MyHomeState extends State<HomePageWidget> {
     }
   }
 }
+
+class NormalListView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        Text("a"),
+        Text("b"),
+        Text("c"),
+        Text("d"),
+        Text("e"),
+        Text("f"),
+        Text("g"),
+        Text("h"),
+      ],
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider(); /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+}
+
